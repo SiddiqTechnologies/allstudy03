09178cd00b6c1528caeb39f7d5f3f12e31a47a0d
Kem Muhammad <siddiqtechnologies@yahoo.com>
2024-04-17 20:45:24 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'allstudy03',
+  webDir: 'dist'
+};
+
+export default config;
