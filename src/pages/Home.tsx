6a8a133b346fb5cd0f9bceb702789808968e440a
Kem Muhammad <siddiqtechnologies@yahoo.com>
2024-04-18 00:00:14 -0400

    Changed Header 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Siddiq Technologies</IonTitle>
+          <IonTitle>Allah U Akbar!!!</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
