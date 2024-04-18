e8fa33445c36a77bb335afc5c5fb3ad2c32df2d8
Kem Muhammad <siddiqtechnologies@yahoo.com>
2024-04-17 23:19:44 -0400

    Updated Home Page

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,8 +5,8 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>Kem Muhammad</strong>
+      <p>Open My Page <a target="_blank" rel="noopener noreferrer" href="https://siddiqtechnologies.com">Siddiq Tech</a></p>
     </div>
   );
 };
