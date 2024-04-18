d526f639366273a544f3abd1ec6f6b89aa354882
Kem Muhammad <siddiqtechnologies@yahoo.com>
2024-04-17 21:06:01 -0400

    Added Live Updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.siddiqtechnologies.allguides',
   appName: 'allstudy03',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: '72974176',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
